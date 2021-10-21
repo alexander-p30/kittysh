eb653a921395570da926568321bc3f589f9a69d5
alexander-p30 <alexanderp30e@gmail.com>
2021-10-21 07:33:24 -0300

    feat: add return code when session file is valid

--- kittysh.sh
+++ kittysh.sh
@@ -4,6 +4,7 @@ chosen_session_file=`ls $session_files_dir | tr ' ' '\n' | fzf`
 
 if [[ $chosen_session_file != "" ]]; then
   kitty --session "$session_files_dir/$chosen_session_file" </dev/null &>/dev/null &!
+  exit 0
 else
   exit 1
 fi
